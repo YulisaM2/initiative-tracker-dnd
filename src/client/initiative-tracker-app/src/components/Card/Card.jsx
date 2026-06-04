877de70ef0fab6885b58b44ad20e0018b48d2a07
Yulisa M. <yulisa.med2@gmail.com>
2026-06-04 00:32:52 -0600

    Loading state used for icon when updating card (key up in text area and after position update for now)

--- src/client/initiative-tracker-app/src/components/Card/Card.jsx
+++ src/client/initiative-tracker-app/src/components/Card/Card.jsx
@@ -3,154 +3,172 @@ import axios from "axios";
 
 import { autoGrow, bringToFront } from "../Card/Card.utils";
 import { DeleteButton } from "../../components/DeleteButton";
+import Spinner from "../../icons/Spinner";
 
 const Card = ({ character, setCharacter }) => {
-  // Tracking general states for query
-  const [loading, setLoading] = useState(true);
-  const [err, setError] = useState(null);
+	// Tracking general states for query
+	const [loading, setLoading] = useState(false);
+	const [err, setError] = useState(null);
+	const keyUpTimer = useRef(null);
 
-  const body = character.genDetails.name;
-  const color = "#FEE5FD";
+	const body = character.genDetails.name;
+	const color = "#FEE5FD";
 
-  // Setup for drag and drop
-  const [position, setPosition] = useState(character.position);
-  let mouseStartPos = { x: 0, y: 0 };
-  const cardRef = useRef(null);
+	// Setup for drag and drop
+	const [position, setPosition] = useState(character.position);
+	let mouseStartPos = { x: 0, y: 0 };
+	const cardRef = useRef(null);
 
-  // Set up for card expansion
-  const textAreaRef = useRef(null);
+	// Set up for card expansion
+	const textAreaRef = useRef(null);
 
-  // So that we can use autoGrow on load (adjust size if prefilled db)
-  useEffect(() => {
-    autoGrow(textAreaRef);
-  }, []);
+	// So that we can use autoGrow on load (adjust size if prefilled db)
+	useEffect(() => {
+		autoGrow(textAreaRef);
+	}, []);
 
-  // For calculating and updating position of a card
-  // Consists of 2 things:
-  // 1. Dragging card (check where it is, where it ended and update positiong)
-  // 2. Letting go (should release note and stop updating position)
+	// For calculating and updating position of a card
+	// Consists of 2 things:
+	// 1. Dragging card (check where it is, where it ended and update positiong)
+	// 2. Letting go (should release note and stop updating position)
 
-  // 1. Clicking and moving card
-  const mouseMove = (e) => {
-    const mouseMoveDir = {
-      // Calculating position, need to know how much was the card moved
-      // Where it ended - from where it was dragged
-      x: mouseStartPos.x - e.clientX,
-      y: mouseStartPos.y - e.clientY,
-    };
+	// 1. Clicking and moving card
+	const mouseMove = (e) => {
+		const mouseMoveDir = {
+			// Calculating position, need to know how much was the card moved
+			// Where it ended - from where it was dragged
+			x: mouseStartPos.x - e.clientX,
+			y: mouseStartPos.y - e.clientY,
+		};
 
-    // Update position for next move
-    mouseStartPos.x = e.clientX;
-    mouseStartPos.y = e.clientY;
+		// Update position for next move
+		mouseStartPos.x = e.clientX;
+		mouseStartPos.y = e.clientY;
 
-    // Update card position
-    const newPos = setNewOffset(cardRef.current, mouseMoveDir);
-    setPosition(newPos);
-  };
+		// Update card position
+		const newPos = setNewOffset(cardRef.current, mouseMoveDir);
+		setPosition(newPos);
+	};
 
-  // Limiting so card can not leave border of screen
-  const setNewOffset = (card, mouseMoveDir = { x: 0, y: 0 }) => {
-    const offsetLeft = card.offsetLeft - mouseMoveDir.x;
-    const offsetTop = card.offsetTop - mouseMoveDir.y;
+	// Limiting so card can not leave border of screen
+	const setNewOffset = (card, mouseMoveDir = { x: 0, y: 0 }) => {
+		const offsetLeft = card.offsetLeft - mouseMoveDir.x;
+		const offsetTop = card.offsetTop - mouseMoveDir.y;
 
-    return {
-      x: offsetLeft < 0 ? 0 : offsetLeft,
-      y: offsetTop < 0 ? 0 : offsetTop,
-    };
-  };
+		return {
+			x: offsetLeft < 0 ? 0 : offsetLeft,
+			y: offsetTop < 0 ? 0 : offsetTop,
+		};
+	};
 
-  const mouseDown = (e) => {
-    // Clicking so it is selecting the card
-    setCharacter(character);
+	const mouseDown = (e) => {
+		// Clicking so it is selecting the card
+		setCharacter(character);
 
-    // This is start poisiton
-    mouseStartPos.x = e.clientX;
-    mouseStartPos.y = e.clientY;
+		// This is start poisiton
+		mouseStartPos.x = e.clientX;
+		mouseStartPos.y = e.clientY;
 
-    bringToFront(cardRef.current);
+		bringToFront(cardRef.current);
 
-    const handleMouseMove = (e) => {
-      mouseMove(e);
-    };
+		const handleMouseMove = (e) => {
+			mouseMove(e);
+		};
 
-    // 2. Dropping and cleanup
-    const handleMouseUp = () => {
-      document.removeEventListener("mousemove", handleMouseMove);
-      document.removeEventListener("mouseup", handleMouseUp);
+		// 2. Dropping and cleanup
+		const handleMouseUp = () => {
+			document.removeEventListener("mousemove", handleMouseMove);
+			document.removeEventListener("mouseup", handleMouseUp);
 
-      const newPos = setNewOffset(cardRef.current);
-      updateCardPos(newPos);
-    };
+			const newPos = setNewOffset(cardRef.current);
+			updateCardPos(newPos);
+		};
 
-    // Preparing as we are clicking to check for either movement or drop
-    document.addEventListener("mousemove", handleMouseMove);
-    document.addEventListener("mouseup", handleMouseUp);
-  };
+		// Preparing as we are clicking to check for either movement or drop
+		document.addEventListener("mousemove", handleMouseMove);
+		document.addEventListener("mouseup", handleMouseUp);
+	};
 
-  // Add persistance to the position of the cards by updating model
-  const updateCardPos = async (newPos) => {
-    try {
-      // Format payload
-      const payload = {
-        position: {
-          x: newPos.x,
-          y: newPos.y,
-        },
-      };
+	// Add persistance to the position of the cards by updating model
+	const updateCardPos = async (newPos) => {
+		try {
+			// Format payload
+			const payload = {
+				position: {
+					x: newPos.x,
+					y: newPos.y,
+				},
+			};
 
-      // Ping db
-      console.log(payload);
-      const response = await axios.patch(
-        `${import.meta.env.VITE_API_CHAR_URL}/${character._id}`,
-        payload,
-      );
+			// Ping db
+			const response = await axios.patch(
+				`${import.meta.env.VITE_API_CHAR_URL}/${character._id}`,
+				payload,
+			);
+		} catch (error) {
+			setError(error.message);
+		} finally {
+			setLoading(false);
+		}
+	};
 
-      // 2. Log out the actual response data here safely
-      console.log("Position updated:", response.data);
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setLoading(false);
-    }
-  };
+	// As we update any area in the card, trigger auto save
+	const handleKeyUp = async () => {
+		setLoading(true);
+		const delay = 1000; // ms
 
-  return (
-    <div
-      className="card"
-      ref={cardRef}
-      style={{
-        backgroundColor: color,
-        left: `${position.x}px`,
-        top: `${position.y}px`,
-      }}
-      onMouseDown={(e) => {
-        // Updating context as focus shifted with click
-        mouseDown(e);
-      }}
-      onFocus={() => {
-        bringToFront(cardRef.current);
-        setCharacter(character);
-      }}
-    >
-      <div className="card-header" style={{ backgroundColor: "#FED0FD" }}>
-        <DeleteButton
-          className="test"
-          id={character._id}
-          setCharacter={setCharacter}
-        />
-      </div>
-      <div className="card-body">
-        <textarea
-          ref={textAreaRef}
-          style={{ color: " #000000" }}
-          defaultValue={body}
-          onInput={() => {
-            autoGrow(textAreaRef);
-          }}
-        ></textarea>
-      </div>
-    </div>
-  );
+		// If we were already tracking, restart as new input was introduced
+		if (keyUpTimer.current) clearTimeout(keyUpTimer.current);
+
+		// Set card update after delay
+		keyUpTimer.current = setTimeout(() => {
+			updateCardPos(cardRef.current);
+		}, delay);
+	};
+
+	return (
+		<div
+			className='card'
+			ref={cardRef}
+			style={{
+				backgroundColor: color,
+				left: `${position.x}px`,
+				top: `${position.y}px`,
+			}}
+			onMouseDown={(e) => {
+				// Updating context as focus shifted with click
+				mouseDown(e);
+			}}
+			onFocus={() => {
+				bringToFront(cardRef.current);
+				setCharacter(character);
+			}}
+		>
+			<div className='card-header' style={{ backgroundColor: "#FED0FD" }}>
+				<DeleteButton
+					className='test'
+					id={character._id}
+					setCharacter={setCharacter}
+				/>
+				{loading && (
+					<div className='card-saving'>
+						<Spinner color={"#000000"} />
+					</div>
+				)}
+			</div>
+			<div className='card-body'>
+				<textarea
+					ref={textAreaRef}
+					style={{ color: " #000000" }}
+					defaultValue={body}
+					onInput={() => {
+						autoGrow(textAreaRef);
+					}}
+					onKeyUp={handleKeyUp}
+				></textarea>
+			</div>
+		</div>
+	);
 };
 
 export default Card;
